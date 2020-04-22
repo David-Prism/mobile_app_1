23fba85213b4647e8acb0822f27e83d3217845bd
davidprismantas@gmail.com <dprismantas@yahoo.com>
2020-04-21 23:22:13 -0700

    Init commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.my_image_view);
+        myImageView.setImageResource(R.drawable.rocket);
+    }
+}
