9ac86128a1d573493dc7f62dbbd554e72ac33c62
davidprismantas@gmail.com <dprismantas@yahoo.com>
2020-05-16 02:00:33 -0700

    assignment 4, added tests

--- app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
@@ -1,36 +1,43 @@
-//package com.example.helloworld;
-//
-//import android.content.Intent;
-//import androidx.test.ext.junit.runners.AndroidJUnit4;
-//import androidx.test.rule.ActivityTestRule;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import static androidx.test.espresso.matcher.ViewMatchers.withText;
-//import static androidx.test.espresso.matcher.ViewMatchers.withId;
-//import static androidx.test.espresso.assertion.ViewAssertions.matches;
-//import static androidx.test.espresso.Espresso.onView;
-//import java.util.Date;
-//import org.junit.runner.RunWith;
-//
-//import static androidx.test.espresso.contrib.PickerActions.setDate;
-//
-//@RunWith(AndroidJUnit4.class)
-//public class MainActivityTest {
-//
-//    @Rule
-//    public ActivityTestRule<MainActivity> activityTestRule
-//            = new ActivityTestRule<MainActivity>(MainActivity.class) {
-//    };
-//
-//
-//    @Test
-//    public void retainsStateAfterRotate() {
-//        // Change state of the button
-//        onView(withId(R.id.text_view_4)).perform(setDate(2017, 6, 30));
-//
-//        TestUtils.rotateScreen(activityTestRule.getActivity());
-//
-//        // Ensure change is still there
-//        onView(withId(R.id.text_view_7)).check(matches(withText("2017/6/30")));
-//    }
-//}
+package com.example.helloworld;
+
+import android.content.Intent;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.Espresso.onView;
+import java.util.Date;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.contrib.PickerActions.setDate;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule
+            = new ActivityTestRule<MainActivity>(MainActivity.class) {
+    };
+
+    @Test
+    public void hasTextOnScreen() {
+        onView(withId(R.id.text_view_1)).check(matches(withText(R.string.name)));
+    }
+
+
+
+
+    @Test
+    public void retainsStateAfterRotate() {
+        // Change state of the button
+        onView(withId(R.id.text_view_4)).perform(setDate(2017, 6, 30));
+
+        TestUtils.rotateScreen(activityTestRule.getActivity());
+
+        // Ensure change is still there
+        onView(withId(R.id.text_view_7)).check(matches(withText("2017/6/30")));
+    }
+}
--- app/src/androidTest/java/com/example/helloworld/ThankYouTest.java
+++ app/src/androidTest/java/com/example/helloworld/ThankYouTest.java
@@ -1,42 +1,42 @@
-//package com.example.helloworld;
-//
-//import static org.junit.Assert.*;
-//
-//import android.content.Intent;
-//import androidx.test.ext.junit.runners.AndroidJUnit4;
-//import androidx.test.rule.ActivityTestRule;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import static androidx.test.espresso.matcher.ViewMatchers.withText;
-//import static androidx.test.espresso.matcher.ViewMatchers.withId;
-//import static androidx.test.espresso.assertion.ViewAssertions.matches;
-//import static androidx.test.espresso.Espresso.onView;
-//import java.util.Date;
-//import org.junit.runner.RunWith;
-//
-//import static androidx.test.espresso.contrib.PickerActions.setDate;
-//
-//@RunWith(AndroidJUnit4.class)
-//public class ThankYouTest {
-//
-//    @Rule
-//    public ActivityTestRule<ThankYou> activityTestRule
-//            = new ActivityTestRule<ThankYou>(ThankYou.class) {
-//        @Override
-//        protected Intent getActivityIntent() {
-//            Intent testIntent = new Intent();
-//            testIntent.putExtra(Constants.KEY_NAME, "David");
-//            testIntent.putExtra(Constants.KEY_EMAIL, "david@gmail.com");
-//            testIntent.putExtra(Constants.KEY_USERNAME, "David1");
-//            testIntent.putExtra(Constants.KEY_DATE, "2020-04-17");
-//            onView(withId(R.id.text_view_4)).perform(setDate(1985, 6, 30));
-//            return testIntent;
-//        }
-//    };
-//
-//    @Test
-//    public void setsRightMessageBasedOnIntentExtra() {
-//        onView(withId(R.id.text_view_7))
-//                .check(matches(withText("Thanks for signing up, David1!")));
-//    }
-//}
+package com.example.helloworld;
+
+import static org.junit.Assert.*;
+
+import android.content.Intent;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.Espresso.onView;
+import java.util.Date;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.contrib.PickerActions.setDate;
+
+@RunWith(AndroidJUnit4.class)
+public class ThankYouTest {
+
+    @Rule
+    public ActivityTestRule<ThankYou> activityTestRule
+            = new ActivityTestRule<ThankYou>(ThankYou.class) {
+        @Override
+        protected Intent getActivityIntent() {
+            Intent testIntent = new Intent();
+            testIntent.putExtra(Constants.KEY_NAME, "David");
+            testIntent.putExtra(Constants.KEY_EMAIL, "david@gmail.com");
+            testIntent.putExtra(Constants.KEY_USERNAME, "David1");
+            testIntent.putExtra(Constants.KEY_DATE, "2020-04-17");
+            onView(withId(R.id.text_view_4)).perform(setDate(1985, 6, 30));
+            return testIntent;
+        }
+    };
+
+    @Test
+    public void setsRightMessageBasedOnIntentExtra() {
+        onView(withId(R.id.text_view_7))
+                .check(matches(withText("David1")));
+    }
+}
