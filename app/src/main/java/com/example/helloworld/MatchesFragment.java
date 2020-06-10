82343a7e6b887bbadf1207a4158d9e99106c8e16
David Prismantas <davidprismantas@gmail.com>
2020-06-09 18:25:41 -0700

    assignment 8 init commit

--- app/src/main/java/com/example/helloworld/MatchesFragment.java
+++ app/src/main/java/com/example/helloworld/MatchesFragment.java
@@ -1,12 +1,21 @@
 package com.example.helloworld;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
+
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,17 +24,22 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MatchesFragment extends Fragment {
 
     private static final String TAG = MatchesFragment.class.getSimpleName();
 
-
+    private LocationManager locationManager;
+    private double longitudeNetwork, latitudeNetwork;
+    private TextView longitudeValueNetwork, latitudeValueNetwork;
 
     @Nullable
     @Override
@@ -33,6 +47,10 @@ public class MatchesFragment extends Fragment {
 
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view,
                 container, false);
+
+        locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        longitudeValueNetwork = getView().findViewById(R.id.longitudeValueNetwork);
+        latitudeValueNetwork = getView().findViewById(R.id.latitudeValueNetwork);
 
         ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
         recyclerView.setAdapter(adapter);
@@ -127,4 +145,72 @@ public class MatchesFragment extends Fragment {
             return mLength;
         }
     }
+
+    // Location services methods -------------------------------
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled()) {
+            showAlert(getContext());
+        }
+        return isLocationEnabled();
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private void showAlert(Context context) {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(Objects.requireNonNull(getContext()));
+        dialog.setTitle(R.string.enable_location)
+                .setMessage(context.getResources().getString(R.string.location_message))
+                .setPositiveButton(R.string.location_settings, (paramDialogInterface, paramInt) -> {
+                    Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(myIntent);
+                })
+                .setNegativeButton(R.string.cancel, (paramDialogInterface, paramInt) -> {});
+        dialog.show();
+    }
+
+    public void toggleNetworkUpdates(View view) {
+        if(!checkLocation()) {
+            return;
+        }
+        Button button = (Button) view;
+        if(button.getText().equals(getResources().getString(R.string.pause))) {
+            locationManager.removeUpdates(locationListenerNetwork);
+            button.setText(R.string.resume);
+        }
+        else {
+            if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60 * 1000, 10, locationListenerNetwork);
+                Toast.makeText(getContext(), getString(R.string.network_provider_started_running), Toast.LENGTH_LONG).show();
+                button.setText(R.string.pause);
+            }
+        }
+    }
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeNetwork = location.getLongitude();
+            latitudeNetwork = location.getLatitude();
+
+            getActivity().runOnUiThread(()-> {
+                longitudeValueNetwork.setText(String.format("%s", longitudeNetwork));
+                latitudeValueNetwork.setText(String.format("%s", latitudeNetwork));
+                Toast.makeText(getContext(), R.string.network_provider_update, Toast.LENGTH_SHORT).show();
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+        @Override
+        public void onProviderEnabled(String s) {}
+
+        @Override
+        public void onProviderDisabled(String s) {}
+    };
+
 }
